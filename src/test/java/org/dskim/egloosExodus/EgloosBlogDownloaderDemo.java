f0b9dca7d3342b5116c0feace76a839247da88c7
bluedskim <bluedskim@gmail.com>
2019-05-23 18:07:27 +0900

    이미지 width 100% 제거

--- src/test/java/org/dskim/egloosExodus/EgloosBlogDownloaderDemo.java
+++ src/test/java/org/dskim/egloosExodus/EgloosBlogDownloaderDemo.java
@@ -16,11 +16,18 @@ public class EgloosBlogDownloaderDemo implements CommandLineRunner {
 	//String blogName = "하고 싶은 걸 하세요 Do What You Want";
 	//String blogBaseUrl = "http://shed.egloos.com/1207526";
 
-	String blogName = "산바람이 만난 자연의 친구들";
-	String blogBaseUrl = "http://yeohans.egloos.com/5979275";	// 사진이 넘 많다.
+	//String blogName = "산바람이 만난 자연의 친구들";
+	//String blogBaseUrl = "http://yeohans.egloos.com/5979275";	// 사진이 넘 많다.
 
 	//String blogName = "플로렌스의 네티하비 블로그";
 	//String blogBaseUrl = "http://netyhobby.egloos.com/1025728";
+
+	// 날짜가 없다?
+	//String blogName = "무릉도원에서 삼라만담";
+	//String blogBaseUrl = "http://atonal.egloos.com/391150";
+
+	String blogName = "라이트노벨 레이블 노블엔진";
+	String blogBaseUrl = "http://novelengin.egloos.com/1327230";
 
 	@Autowired
 	EgloosBlogDownloader egloosBlogDownloader;
